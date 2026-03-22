c43f6c1c0ed111c741ec445349609a11e0303c2d
maximeg100 <maximeguilbert123@gmail.com>
2026-03-22 20:36:18 +0100

    analyser_liste / est_du_texte / analyser_item / analyser_texte

--- analyseur.c
+++ analyseur.c
@@ -127,9 +127,88 @@ t_noeud* analyser_titre(){
 }
 
 
+t_noeud* analyser_liste() {
+    consommer_balise(TOKEN_BALISE_OUVRANTE, "liste");
+    t_noeud* mon_noeud_lst = creer_noeud(TYPE_LISTE);
+    t_noeud* premier_item = NULL;
+    t_noeud* dernier_item = NULL;
+    while (mon_token_courant.mon_type == TOKEN_BALISE_OUVRANTE && strcmp(mon_token_courant.ma_valeur, "item") == 0) {
+        t_noeud* nouvel_item = analyser_item();
+        if (premier_item == NULL) {
+            premier_item = nouvel_item;
+            mon_noeud_lst->mon_premier_fils = premier_item;
+        } else {
+            dernier_item->mon_frere_suivant = nouvel_item;
+        }
+        dernier_item = nouvel_item;
+    }
+    consommer_balise(TOKEN_BALISE_FERMANTE, "liste");
+    return mon_noeud_lst;
+}
+
+// Fonction utilitaire pour savoir si c'est du texte
+int est_du_texte() {
+    // 1. Texte brut
+    if (mon_token_courant.mon_type == TOKEN_TEXTE) {
+        return 1;
+    }
+    // 2. Balise <important>
+    if (mon_token_courant.mon_type == TOKEN_BALISE_OUVRANTE && 
+        strcmp(mon_token_courant.ma_valeur, "important") == 0) {
+        return 1;
+    }
+    // 3. Balise <br/>
+    if (mon_token_courant.mon_type == TOKEN_BALISE_AUTO_FERMANTE && 
+        strcmp(mon_token_courant.ma_valeur, "br") == 0) {
+        return 1;
+    }
+    return 0; // Ce n'est pas du texte
+}
+
+t_noeud* analyser_item() {
+    consommer_balise(TOKEN_BALISE_OUVRANTE, "item");
+    t_noeud* mon_noeud_item = creer_noeud(TYPE_ITEM);
+    t_noeud* premier_bloc = NULL;
+    t_noeud* deuxieme_bloc = NULL;
+    // On regarde le premier jeton pour choisir la règle (9 ou 10)
+    if (mon_token_courant.mon_type == TOKEN_BALISE_OUVRANTE && strcmp(mon_token_courant.ma_valeur, "liste") == 0) {
+        // CAS : <liste_texte>
+        premier_bloc = analyser_liste();
+        if (est_du_texte()) {
+            deuxieme_bloc = analyser_texte();
+        }
+    } 
+    else if (est_du_texte()) {
+        // CAS : <texte_liste>
+        premier_bloc = analyser_texte();
+        if (mon_token_courant.mon_type == TOKEN_BALISE_OUVRANTE && strcmp(mon_token_courant.ma_valeur, "liste") == 0) {
+            deuxieme_bloc = analyser_liste();
+        }
+    }
+    // On branche les morceaux sous l'item
+    mon_noeud_item->mon_premier_fils = premier_bloc;
+    if (premier_bloc != NULL) {
+        premier_bloc->mon_frere_suivant = deuxieme_bloc;
+    }
+    consommer_balise(TOKEN_BALISE_FERMANTE, "item");
+    return mon_noeud_item;
+}
+
+t_noeud* analyser_texte() {
+    t_noeud* premier_mot = NULL;
+    t_noeud* dernier_mot = NULL;
+    while (est_du_texte()) {
+        t_noeud* nouveau_mot = analyser_mot_enrichi();
+        if (premier_mot == NULL) {
+            premier_mot = nouveau_mot;
+        } else {
+            dernier_mot->mon_frere_suivant = nouveau_mot;
+        }
+        dernier_mot = nouveau_mot;
+    }
+    return premier_mot;
+}
 
 
 
-
-
-//autres fonctions (analyser_section, analyser_liste, etc.)
+//autres fonctions 
